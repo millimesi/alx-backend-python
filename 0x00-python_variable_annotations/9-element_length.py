80e220438e896612a0bf054898751abe07db48b9
millimesi <millionmesi1@gmail.com>
2024-06-15 13:19:26 +0300

    Documentation is added

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,4 +5,5 @@ from typing import Sequence, Iterable, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    ''' Duck type annotation in seqquence and Iterable'''
     return [(i, len(i)) for i in lst]
