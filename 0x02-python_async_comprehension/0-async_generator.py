bb989ebbdbdda6c73b92f66bfa62b91d7329a6a2
millimesi <millionmesi1@gmail.com>
2024-06-18 12:22:36 +0300

    Annotation and importing problem fixed

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     ''' Generate async expresion 10 times in every 10 sec'''
     for _ in range(10):
         await asyncio.sleep(1)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,17 +3,9 @@
 
 import random
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
-
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     ''' using async for'''
-    async def measure_runtime() -> float:
-    """
-    Async function that measures total run time
-    and return it
-    """
-    start = time.perf_counter()
-    await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    end = time.perf_counter()
-    return end - start
+    return [i async for i in async_generator()]
